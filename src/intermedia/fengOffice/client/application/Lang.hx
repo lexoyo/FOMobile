77a5ffc3d4265761bc20fbcb727256180e1d6a9f
lexa <a.hoyau@silexlabs.org>
2012-04-25 17:03:16 +0200

    translation module and visual additions

---
+++ src/intermedia/fengOffice/client/application/Lang.hx
@@ -0,0 +1,30 @@
+package intermedia.fengOffice.client.application;
+
+/**
+ * translation for the client app
+ */
+class Lang {
+	////////////////////////////////
+	// text in the item list 
+	////////////////////////////////
+	public static var ITEM_SELECT:String = "View";
+	public static var WORKSPACE_SELECT:String = "Choose";
+	public static var ITEM_OPEN:String = "Info";
+	////////////////////////////////
+	// login page
+	////////////////////////////////
+	public static var LOGIN_BTN:String = "Login";
+	public static var PASS_LABEL:String = "Password:";
+	public static var USERNAME_LABEL:String = "Email or username:";
+	////////////////////////////////
+	// home page
+	////////////////////////////////
+	public static var CURRENT_WORKSPACE_LABEL:String = "Current workspace";
+	public static var FILES_LABEL:String = "Files";
+	public static var TASKS_LABEL:String = "Tasks";
+	public static var NOTES_LABEL:String = "Notes";
+	public static var EMAILS_LABEL:String = "Emails";
+	public static var LINKS_LABEL:String = "Links";
+	public static var CALENDAR_LABEL:String = "Calendar";
+	public static var CONTACTS_LABEL:String = "Contacts";
+}
